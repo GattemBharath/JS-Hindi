c52d706457cfc835fa6eb3ffb9dc971eafa08d6a
Gattem Bharath <53399475+GattemBharath@users.noreply.github.com>
2023-09-10 14:06:10 +0530

    arrays part-1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,41 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+
+const myHeros = ["shinchan", "hattori", "doraemon"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+
+//*  Array Methods
+
+myArr.push(6);
+myArr.push(7);
+console.log(myArr); //! [0, 1, 2, 3, 4, 5, 6, 7];
+
+myArr.pop();
+console.log(myArr); //! [0, 1, 2, 3, 4, 5, 6];
+
+myArr.unshift(9);
+console.log(myArr); //! [9, 0, 1, 2, 3, 4, 5, 6];
+
+myArr.shift(9);
+console.log(myArr); //! [0, 1, 2, 3, 4, 5, 6];
+
+console.log(myArr.includes(5)); //! true
+
+console.log(myArr.indexOf(9)); //! -1
+console.log(myArr.indexOf(3)); //! 3
+
+const newArr = myArr.join();
+console.log(newArr); //! 0,1,2,3,4,5,6
+console.log(typeof newArr); //! string
+
+//? slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1); //! [1, 2]
+console.log("B", myArr); //! B [ 0, 1, 2, 3, 4, 5, 6]
+
+const myn2 = myArr.splice(1, 3);
+console.log(myn2); //! [1, 2, 3]
+console.log("C", myArr); //! C [ 0, 4, 5, 6 ]
